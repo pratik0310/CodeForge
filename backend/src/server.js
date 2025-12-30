6f6404be28e6e2553945a0acd23eb4ba7fa2f97c
Pratik <ghargepratik111@gmail.com>
2025-12-30 16:19:13 +0530

    updated server js

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,6 +24,7 @@ app.get("/{*any}",(req,res)=>{
     res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
 })
 }
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
